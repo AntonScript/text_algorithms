737901b42525f1b4fc9cf59d56e6ec27a99885f7
Мамакин Антон Сергеевич <anton.mamakin@nordclan.com>
2022-04-16 19:16:56 +0400

    start

---
+++ src/main/java/com/example/text_algorithms/TextAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.text_algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextAlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/text_algorithms/TextAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.text_algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
